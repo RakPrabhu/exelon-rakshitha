af91c5ddea98ea06b7b4b1e40a05dfa4ec085e99
Rakshitha Prabhu <rakshithaprabhu03@gmail.com>
2025-02-15 15:09:56 +0530

    twilio_api

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
-
+import dotenv from 'dotenv';
+dotenv.config()
 const app = express();
 const port = 3000;
 
@@ -10,3 +11,16 @@ app.get('/', (req: Request, res: Response) => {
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
+
+
+import { TwilioService } from './services/twilio'; // Import the service
+
+const testTwilio = async () => {
+const testPhoneNumber = process.env.RECIPIENT_PHONE_NUMBER || ''; // Replace with your real number
+  const testMessage = 'Your OTP for exelon is 5559';
+
+  const result = await TwilioService.sendSMS(testPhoneNumber, testMessage);
+  console.log('Test Result:', result);
+};
+
+testTwilio();
